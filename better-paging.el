1346bc6f75586799c13a193b0c8aef398e838b2c
Mark W. Naylor <mark.naylor.1701@gmail.com>
2024-04-19 14:55:43 -0400

    Improve page-up and page-down
    * Eliminate complicated abstract scroll function.
    * Add support functions.

--- better-paging.el
+++ better-paging.el
@@ -4,51 +4,34 @@
 
 ;; Author: Mark W. Naylor <mark.naylor.1701@gmail.com>
 ;; Version: 0.9
-;; Package-Requires: ((emacs "26.0"))
+;; Package-Requires: ((emacs "26.0") (cl-lib "1.0") (info) (locate))
 ;; Keywords: navigation
 ;; file:  better-paging.el
 ;; date:  2021-Feb-10
 
-(defun page-scroll (new-window-line anchor-fn point-compare-fn
-                    point-at-line-number scroll-fn)
-  (let* ((start-point (point))
-         (_ (move-to-window-line new-window-line))
-         (_ (funcall anchor-fn))
-        (new-point (point)))
-    (if (funcall point-compare-fn start-point new-point)
-        (progn
-          (recenter point-at-line-number)
-          new-point)
-      (progn
-        (goto-char start-point)
-        (condition-case erc
-         (funcall scroll-fn)
-         (error (bp-scroll-erc-handler erc)))))))
+(require 'cl-lib)
+(require 'info)
+(require 'locate)
 
-(defun bp-scroll-erc-handler (erc)
-  (let ((msg (car erc)))
-    (if (not
-         (or (string= msg "end-of-buffer")
-             (string= msg "beginning-of-buffer")))
-        (error msg))))
-
-(defun page-up (&optional anchor-fn)
+(defun page-up (&optional move-point-fn)
   (interactive)
-  (page-scroll
-   -1
-   (or anchor-fn #'backward-paragraph)
-   #'<
-   0
-   #'scroll-up-command))
+  (when (> (cl-first (page--count-lines-page)) (line-number-bottom-window))
+    (let ((recenter-positions '(bottom)))
+      (move-to-window-line-top-bottom))
+    (funcall (or move-point-fn #'backward-paragraph))
+    (let ((recenter-positions '(top)))
+      (recenter-top-bottom))
+    (point)))
 
-(defun page-down (&optional anchor-fn)
+(defun page-down (&optional move-point-fn)
   (interactive)
-  (page-scroll
-   1
-   (or anchor-fn #'forward-paragraph)
-   #'>
-   -1
-   #'scroll-down-command))
+  (when (> (line-number-top-window) (bp--top-line))
+    (let ((recenter-positions '(top)))
+      (move-to-window-line-top-bottom))
+    (funcall (or move-point-fn #'forward-paragraph))
+    (let ((recenter-positions '(bottom)))
+      (recenter-top-bottom))
+    (point)))
 
 (defun info-page-up ()
   "Like `page-up', except that it will jump to the next info node
@@ -77,6 +60,28 @@ when at the top of the current page."
 (defun erc-page-up ()
   (interactive)
   (page-up #'prior-erc-tag))
+
+(defun line-number-window-relative (number-or-symbol)
+  (save-excursion
+    (let* ((recenter-positions (list number-or-symbol)))
+      (move-to-window-line-top-bottom)
+      (locate-current-line-number))))
+
+(defun line-number-bottom-window ()
+  (line-number-window-relative 'bottom))
+
+(defun line-number-top-window ()
+  (line-number-window-relative 'top))
+
+(defun displayed-lines ()
+  (- (line-number-bottom-window) (line-number-top-window) -1))
+
+;; Hack because M-r, `move-to-window-line-top-bottom', jumps to the /second/
+;; line, not the first.
+(defun bp--top-line ()
+  (cond
+   ((string= major-mode "Info-mode") 2)
+   (t 1)))
 
 (provide 'better-paging)
 ;; ------------------------------------------------------------------------------
