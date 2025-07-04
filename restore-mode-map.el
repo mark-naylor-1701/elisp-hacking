15452c862a2408a4185d3ee03f1ade11fb6296dc
Mark W. Naylor <mark.naylor.1701@gmail.com>
2025-07-04 17:58:30 -0400

    Added
    * Documentation
    ** Comments
    ** Org file
    
    * Make the command more user friendly
    ** Restores window state after processing.
    ** Kill any buffers that were created during processing.

--- restore-mode-map.el
+++ restore-mode-map.el
@@ -1,8 +1,8 @@
-;;; restore-mode-map.el --- Fix keymaps for major (or minor) modes. -*- lexical-binding: t -*-
+;;; restore-mode-map.el --- Fix keymaps for major (or minor) modes. -*- lexical-binding: t; -*-
 
 ;; Author: Mark W. Naylor <mark.naylor.1701@gmail.com>
 ;; Version: 0.9
-;; Package-Requires: ((emacs "30.0") (obarray-ext "0.9") (s "20220902.1511")
+;; Package-Requires: ((emacs "30.0") (obarray-ext "0.9") (s "20220902.1511"))
 ;; Keywords: mode map keybinding
 ;; URL: https://github.com/mark-naylor-1701/elisp-hacking/restore-mode-map.el
 ;; date:  2025-Jun-07
@@ -10,48 +10,49 @@
 ;;; Commentary:
 ;;
 
-
 
 ;;; Requires:
+(require 'help-mode)
+(require 's)
+
 (require 'obarray-ext)
 
 
 ;;; Code:
 
-(defun rmm-restore (arg)
-  "docstring"
+(defun rmm-restore (prefix)
+  "Restore mode keybindings. With no prefix, restore the key map
+for the current major mode. With a prefix, prompt the user for the mode
+map to be restored. A list of currently registerd mode maps is
+presented to make selection easier."
   (interactive "P")
   (rmm--restore
    (cond
-    (arg (rmm--select-mode-map))
-    (t (concat (symbol-name major-mode) "-map")))))
-
+    (prefix (rmm--select-mode-map))
+    (t      (concat (symbol-name major-mode) "-map")))))
 
 
 ;; Private
 
-(defun rmm--restore (mode-map-name-str)
-  "When `MODE-NAME-STR' matches a major or minor mode, reset its key map to the
-original definition."
-  (when-let* ((mode-sym (intern-soft mode-map-name-str)))
-    (when (boundp mode-sym)
+(defun rmm--restore (map-name)
+  "When `MAP-NAME' matches a key map of a major or minor mode, reset the
+key map to the original definition."
+  (when-let* ((mode-sym       (intern-soft map-name))
+              (_              (boundp mode-sym))
+              (before-buffers (buffer-list)))
+    (save-window-excursion
       (describe-variable mode-sym)
-      (switch-to-buffer "*Help*")
-      (help-view-source)
-      ;;(forward-sexp)
-      ;;(eval-last-sexp)
-      (forward-char)
-      (eval-defun nil)
-      (kill-buffer-and-window)
-      ;;(switch-to-buffer "*Help*")
-      (select-window (next-window))
-      (help-go-back)
-      (delete-window))))
+      (with-current-buffer "*Help*"
+       (help-view-source)
+       (eval-defun nil))
+      (with-current-buffer "*Help*"
+        (help-go-back))
+      (mapc #'kill-buffer
+            (cl-set-difference (buffer-list) before-buffers)))))
 
 (defun rmm--mode-map-name-p (str)
   "Determine if `STR' has a name that matches a mode map pattern."
   (s-ends-with? "-mode-map" str))
-
 
 (defun rmm--mode-map-symbol-p (sym)
   "Determine if `SYM' matches a mode map variable pattern."
