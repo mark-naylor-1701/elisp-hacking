56f6fcb8a958c4b24a02e27eea1c912263dd5f66
Mark W. Naylor <mark.naylor.1701@gmail.com>
2025-07-12 12:48:12 -0400

    Functions extracted from the old better-paging.el package.

--- window-ext.el
+++ window-ext.el
@@ -1,19 +1,90 @@
-;;; -*- lexical-binding: t -*-
-;;; window-ext.el --- DESCRIPTION
-
-;;; Commentary:
-;;
-
-
+;;; window-ext.el --- Enhancement to emacs core.  -*- lexical-binding: t -*-
 
 ;; Author: Mark W. Naylor <mark.naylor.1701@gmail.com>
 ;; Version: 0.9
-;; Package-Requires: ((emacs "30.0"))
+;; Package-Requires: ((emacs "30.0") (locate))
 ;; Keywords: foo bar baz
 ;; URL: http://example.com/jrhacker/superfrobnicate
-;; author: Mark W. Naylor
-;; file:  window-ext.el
-;; date:  2025-Jul-12
+
+;;; Commentary:
+;;  Extentions of the window move line and recenter functions.
+
+;;; Code
+
+;; Requires
+
+(require 'locate)
+
+
+;; Values
+
+(defvar-local we--top '(top))
+(defvar-local we--bottom '(bottom))
+
+
+
+;; Functions
+
+(defun we-move-to-window-line-bottom ()
+  "Set the cursor on the first line of the current window."
+  (let ((recenter-positions we--bottom))
+    (move-to-window-line-top-bottom)))
+
+(defun we-move-to-window-line-top ()
+  "Set the cursor on the last line of the current window."
+  (let ((recenter-positions we--top))
+    (move-to-window-line-top-bottom)))
+
+(defun we-recenter-bottom ()
+  "Move the current line to the bottom of the window."
+  (let ((recenter-positions we--bottom))
+    (recenter-top-bottom)))
+
+(defun we-recenter-top ()
+  "Move the current line to the top of the window."
+  (let ((recenter-positions we--top))
+    (recenter-top-bottom)))
+
+(defun we-goto-window-bottom ()
+  "Move to the last char in the window and return the current point."
+  (we-move-to-window-line-bottom)
+  (move-end-of-line nil)
+  (point))
+
+(defun we-goto-window-top ()
+  "Move to the first char in the window and return the new point."
+  (we-move-to-window-line-top)
+  (point))
+
+(defun we-line-number-window-relative (number-or-symbol)
+  "Return the line numbeber of NUMBER-OR-SYMBOL.
+
+NUMBER-OR-SYMBOL can be an integer or one of the symbols used by
+`recenter-positions'. Restores the point before exiting the function. If an
+number, it will be relative to the top line if positive and relative to the
+bottom line if negative. If number exceeds the lines in the buffer, returns the
+corresponding top or bottom line of the buffer."
+  (save-excursion
+    (let ((recenter-positions (list number-or-symbol)))
+      (move-to-window-line-top-bottom)
+      (locate-current-line-number))))
+
+(defun we-line-number-bottom-window ()
+  "Return the point of the last line, relative to the current window."
+  (we-line-number-window-relative 'bottom))
+
+(defun we-line-number-top-window ()
+  "Return the point of the first line, relative to the current
+window."
+  (we-line-number-window-relative 'top))
+
+(defun we-displayed-lines ()
+  "Calculate the number of full lines diplayed in the current window."
+  (- (we-line-number-bottom-window) (we-line-number-top-window) -1))
+
+(defun we-narrow-to-window ()
+  "Narrow to the fully displayed lines in the current window."
+  (narrow-to-region (we-goto-window-top) (we-goto-window-bottom)))
 
 (provide 'window-ext)
 ;; ------------------------------------------------------------------------------
@@ -48,5 +119,8 @@
 ;; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ;; https://opensource.org/licenses/BSD-3-Clause
+;; Local Variables:
+;; read-symbol-shorthands: (("we-" . "window-ext-"))
+;; End:
 
 ;;; window-ext.el ends here
